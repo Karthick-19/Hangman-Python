677862f3310b85a617ed196542df9c00ff7e9e84
Karthick-19 <103735562+Karthick-19@users.noreply.github.com>
2022-06-07 23:24:03 +0530

    Add files via upload

---
+++ Hangman/words.py
@@ -0,0 +1,5 @@
+word=['dummy','pussy','chest',"hi","cat","home","country","computer","irc","hat","wow","Berlin","fly","airplane","cow","mapsmake","run","sigh","golf","running","hat","television","games","hangman","pearl","cat","dog","snake","black","white",
+"cabin",
+"trees",
+"birds",
+"animals",]
